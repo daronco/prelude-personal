7517553a273a5fc2335008caddbf8f42fe6c91b3
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-07-27 01:09:13 -0300

    Personal packages
    Install flymake-ruby and flymake-cursor for now.

---
+++ packages.el
@@ -0,0 +1,24 @@
+; Copied from prelude-packages.el
+; Installs my personal packages
+
+(defvar personal-packages
+  '(flymake-ruby flymake-cursor)
+  "A list of packages to ensure are installed at launch.")
+
+(defun personal-packages-installed-p ()
+  (loop for p in personal-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(defun personal-install-packages ()
+  (unless (personal-packages-installed-p)
+    ;; check for new packages (package versions)
+    (message "%s" "Emacs prelude-personal is now refreshing its package database...")
+    (package-refresh-contents)
+    (message "%s" " done.")
+    ;; install the missing packages
+    (dolist (p personal-packages)
+      (unless (package-installed-p p)
+        (package-install p)))))
+
+(personal-install-packages)
