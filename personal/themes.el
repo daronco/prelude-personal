a94111596e2573440eae74d8aa4505e9baeac8cd
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-07-29 12:13:46 -0300

    Better colors for whitespaces

--- personal/themes.el
+++ personal/themes.el
@@ -2,23 +2,26 @@
 
 ; Customize zenburn
 (let ((class '((class color) (min-colors 89)))
+      (zenburn-fg "#dcdccc")
       (zenburn-bg-1 "#161616")
       (zenburn-bg-05 "#1c1c1c")
       (zenburn-bg "#262626")
+      (zenburn-bg+05 "#434343")
       (zenburn-bg+1 "#4f4f4f")
+      (zenburn-error "#800080")
+      (zenburn-yellow "#E2E28F")
       )
   (custom-theme-set-faces
    'zenburn
    `(default ((,class (:background ,zenburn-bg))))
    `(highlight ((,class (:background ,zenburn-bg-05))))
    `(mode-line-inactive ((,class (:background ,zenburn-bg-05))))
-   `(whitespace-line ((,class (:background ,zenburn-bg-05))))
-   `(whitespace-space ((,class (:background ,zenburn-bg :foreground ,zenburn-bg+1))))
-   `(whitespace-hspace ((,class (:background ,zenburn-bg :foreground ,zenburn-bg+1))))
-   `(whitespace-tab ((,class (:background ,zenburn-bg))))
-   `(whitespace-newline ((,class (:foreground ,zenburn-bg+1))))
-   `(whitespace-trailing ((,class (:background ,zenburn-bg))))
-   `(whitespace-line ((,class (:background ,zenburn-bg-05))))
+   `(whitespace-space ((,class (:background ,zenburn-bg :foreground ,zenburn-bg+05)))) ; normal spaces
+   `(whitespace-hspace ((,class (:background ,zenburn-bg :foreground ,zenburn-bg+05))))
+   `(whitespace-tab ((,class (:background ,zenburn-bg :foreground ,zenburn-bg+05)))) ; normal tabs
+   `(whitespace-newline ((,class (:foreground ,zenburn-bg+05))))
+   `(whitespace-trailing ((,class (:background ,zenburn-error))))
+   `(whitespace-line ((,class (:background ,zenburn-bg :foreground ,zenburn-yellow)))) ; long lines
    `(region ((,class (:background ,zenburn-bg-1))))
    )
   )
